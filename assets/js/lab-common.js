43193b2017989c939c48d186cd0d1e5a1be22336
Salah Wahsh <63211856+Salah-Wahsh@users.noreply.github.com>
2026-09-18 22:05:56 +0300

    chore: remove Pre-Engagement & Legal RoE section and renumber curriculum

--- assets/js/lab-common.js
+++ assets/js/lab-common.js
@@ -14,7 +14,7 @@
       title: 'Module 01: Fundamentals',
       badge: 'Core',
       labs: [
-        { id: 'lab01', name: 'Lab 01: Legal Frameworks & Web Prerequisites', href: 'labs/lab01.html', time: '50m' }
+        { id: 'lab01', name: 'Lab 01: Security Concepts & Web Prerequisites', href: 'labs/lab01.html', time: '45m' }
       ]
     }
   ];
